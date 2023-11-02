805e6af51322664b3c8db57620d63f73e379887c
AyeshaYNTHV <146338685+AyeshaYNTHV@users.noreply.github.com>
2023-11-02 16:58:57 +0500

    Add files via upload

---
+++ ayesha.............cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<ctime>
+using namespace std;
+
+int main() {
+	int books, days_for_book = 5, fine=0, issue_date=1;
+
+	cout << "Books you want to borrow: ";
+	cin >> books;
+	cout << "Give your days for book";
+	cin >> issue_date;
+	while 
+	
+	return 0;
+
+}
